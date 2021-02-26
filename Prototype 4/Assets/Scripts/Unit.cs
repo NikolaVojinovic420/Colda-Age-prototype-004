9d825496ed15168dd8d8b6cea51c238b2197f885
NikolaVojinovic420 <78732971+NikolaVojinovic420@users.noreply.github.com>
2021-02-26 11:28:58 +0100

    Push 260221 11:28

--- Prototype 4/Assets/Scripts/Unit.cs
+++ Prototype 4/Assets/Scripts/Unit.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Unit : MonoBehaviour
 {
+    public GameObject aspectObject;
     public Aspect aspect;
     public UnitDisplay unitDisplay;
+    public Text txt;
     // Start is called before the first frame update
     void Start()
     {
+        aspect = aspectObject.GetComponent<Aspect>();
         unitDisplay = GameObject.FindWithTag("UnitDisplay").GetComponent<UnitDisplay>();
+        txt.text = $"{gameObject.name}\n{aspect.ReturnAspectString()}";
     }
 
     // Update is called once per frame
