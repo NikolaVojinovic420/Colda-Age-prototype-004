23bfd739b9fd56b45d92bd6d89d48c02dc00fa63
Simon Svane <smnsvane@gmail.com>
2021-03-09 19:11:26 +0100

    removed monobehavior from state added State.End() call to SetState method

--- Prototype 4/Assets/Scripts/State Machine/State.cs
+++ Prototype 4/Assets/Scripts/State Machine/State.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class State : MonoBehaviour
+public abstract class State
 {
     protected StateMachine _stateMachine;
     public State(StateMachine stateMachine)
--- Prototype 4/Assets/Scripts/State Machine/StateMachine.cs
+++ Prototype 4/Assets/Scripts/State Machine/StateMachine.cs
@@ -9,10 +9,12 @@ public class StateMachine : MonoBehaviour
 
     void Awake()
     {
+        Debug.Log("awake statemachine");
         _state = new StartState(this);
     }
     public void SetState(State state)
     {
+        StartCoroutine(_state.End());
         _state = state;
         StartCoroutine(_state.Start());
     }
