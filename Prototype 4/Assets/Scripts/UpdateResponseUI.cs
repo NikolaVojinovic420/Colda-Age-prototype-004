fcfc6e877c5e271451183d552917ba84f5f9819b
NikolaVojinovic420 <78732971+NikolaVojinovic420@users.noreply.github.com>
2021-04-01 16:48:59 +0200

    New update for name separated for unit, response and event. Update of name/title/flavor/story from game object

---
+++ Prototype 4/Assets/Scripts/UpdateResponseUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateResponseUI : MonoBehaviour
+{
+    public TMP_Text textUI,a,p,c;
+    public GameObject loss, win, exhaust, insert;
+    Effect effect;
+    Aspect aspect;
+    void Awake()
+    {
+        effect = GetComponent<Effect>();
+        aspect = GetComponent<Aspect>();
+
+        textUI.text = gameObject.name.Split('(')[0];
+        a.text = $"{aspect.a}";
+        p.text = $"{aspect.p}";
+        c.text = $"{aspect.c}";
+
+
+        if (effect.loss)
+            loss.SetActive(true);
+        if (effect.win)
+            win.SetActive(true);
+        if (effect.exhaustable)
+            exhaust.SetActive(true);
+        if (effect.insertEvent != null)
+            insert.SetActive(true);
+        if (effect.draw > 0)
+            textUI.text += $"\nDraw {effect.draw} units.";
+    }
+}
---
+++ Prototype 4/Assets/Scripts/UpdateUnitUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateUnitUI : MonoBehaviour
+{
+    public TMP_Text nameUI,a,p,c;   
+    void Awake()
+    {
+        nameUI.text = gameObject.name.Split('(')[0];
+        a.text = $"{GetComponent<Aspect>().a}";
+        p.text = $"{GetComponent<Aspect>().p}";
+        c.text = $"{GetComponent<Aspect>().c}";
+    }
+}
+
